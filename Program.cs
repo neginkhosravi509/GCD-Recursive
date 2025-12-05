fbe1dcbdbdc253b23aebf05e49594b578f7c50d7
negin <neginkhosravi509@gmail.com>
2025-12-05 23:23:31 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bozorgtarin_maqsoom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("enter number");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter number");
+            int b = int.Parse(Console.ReadLine());
+            int result = divisor(a, b);
+            Console.WriteLine();
+            Console.WriteLine($"divisor = {result}");
+            Console.ReadKey();
+        }
+        static int divisor(int a, int b)
+        {
+            if (b==0)
+            
+                return a;
+            return divisor(b, a % b);
+        }
+    }
+}
